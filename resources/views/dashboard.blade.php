dbb706cd421627986a13a96179b5683a2297134f
shazidian <158548646+shazidian@users.noreply.github.com>
2025-02-10 03:33:48 +0700

    mengerjakan bkpm 3,4

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Dashboard Penduduk Kauman</title>
+</head>
+<body>
+    <h1>Selamat Datang di Website Diana! Ada yang Bisa Saya Bantu? ^-^</h1>
+</body>
+</html>
