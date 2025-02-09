34cb4e6ca0db7b0ec03657e606627d9bfe3de2c9
shazidian <158548646+shazidian@users.noreply.github.com>
2025-02-10 03:34:28 +0700

    mengerjakan bkpm 3,4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,42 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+//Route View
+Route::view('/dashboard', 'dashboard');
 Route::get('/', function () {
     return view('welcome');
 });
+//Route Default
+Route::get('/users', [UserController::class, 'index']);
+Route::get('users/{id}', [UserController::class, 'show']);
+
+//Contoh Route Redirect
+Route::get('/lama', function(){
+    return "Hai! Ini Laman Web Versi 1.0";
+});
+Route::get('/baru', function(){
+    return "Selamat! Anda berada di Laman Web Versi 2.0";
+});
+Route::redirect('/lama', '/baru');
+
+//Parameter Opsional
+Route::get('/user/{name?}', function ($name = 'Guest') {
+    return "Halo, $name!";
+});
+
+//Route Regular Expression
+Route::get('/barang/{id}', function ($id) {
+    return "ID Barang: $id";
+})->where('id', '[0-9]+');
+
+//Global Constraints at app/providers/RouteServiceProvider.php
+Route::get('/pesan/{id}', function ($id) {
+    return "Nomor Pesanan: $id";
+});
+
+//CSRF
+Route::post('/submit', function ($request) {
+    return 'Form berhasil dikirim!';
+});
+
+
