2ced0741f149823120c2285eeac6a49df7eafa5b
shazidian <158548646+shazidian@users.noreply.github.com>
2025-02-21 10:48:46 +0700

    Update homes.blade.php

--- resources/views/frontend/homes.blade.php
+++ resources/views/frontend/homes.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     </div>
                     <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="100">
-                        <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+                        <img src="frontend/Butterfly/assets/img/hero-img.png" class="img-fluid animated" alt="">
                     </div>
                 </div>
             </div>
