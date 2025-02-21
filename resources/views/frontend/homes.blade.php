872179a2934a6e87eb831aade019729fb66895f0
shazidian <158548646+shazidian@users.noreply.github.com>
2025-02-21 10:12:54 +0700

    update bkpm7

--- resources/views/frontend/homes.blade.php
+++ resources/views/frontend/homes.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts.template')
 @section('content')
         <!-- Hero Section -->
-        <section id="hero" class="hero section light-background">
+        <section id="hero" class="d-flex align-items-center">
 
             <div class="container">
                 <div class="row gy-4">
@@ -14,7 +14,7 @@
                         </div>
                     </div>
                     <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="100">
-                        <img src="frontend/Butterfly/assets/img/hero-img.png" class="img-fluid animated" alt="">
+                        <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
                     </div>
                 </div>
             </div>
--- resources/views/frontend/layouts/navbar.blade.php
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
 
         <a href="index.html" class="logo d-flex align-items-center">
-            <img src="assets/img/logo.png" alt="">
+            <img src="frontend/Butterfly/assets/img/logo.png" alt="">
             <!-- Uncomment the line below if you also wish to use text logo -->
             <!-- <h1 class="sitename">Butterfly</h1>  -->
         </a>
@@ -33,3 +33,10 @@
                         <li><a href="#">Dropdown 4</a></li>
                     </ul>
                 </li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+
+    </div>
+</header>
