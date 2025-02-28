e3c71f553bbfe804d4bd3d827271926aacb53c34
shazidian <158548646+shazidian@users.noreply.github.com>
2025-02-28 12:53:32 +0700

    updateacara13

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -1,4 +1,4 @@
-  <!-- ======= Sidebar ======= -->
+<!-- ======= Sidebar ======= -->
   <aside id="sidebar" class="sidebar">
 
     <ul class="sidebar-nav" id="sidebar-nav">
@@ -10,17 +10,11 @@
         </a>
       </li><!-- End Dashboard Nav -->
       <li class="nav-item">
-        <a class="nav-link " href="index.html">
+        <a class="nav-link collapsed " href="index.html">
           <i class="bi bi-menu-button-wide"></i>
           <span>Profile</span>
         </a>
       </li><!-- End Dashboard Nav -->
-
-      {{-- <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Profile</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-      </li><!-- End Components Nav --> --}}
 
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
@@ -44,3 +38,35 @@
     </ul>
 
   </aside><!-- End Sidebar-->
+
+  {{-- <aside>
+    <div id="sidebar" class="nav-collase">
+        <ul class="sidebar-menu">
+            <li class="active">
+            <a class="" href="index.html">
+                <i class="icon_house_alt"></i>
+                <span>Dashboard</span>
+            </a>
+
+            </li>
+            <li>
+                <a class="" href="index.html">
+                    <i class="icon_profile"></i>
+                    <span>Profile</span>
+                </a>
+            </li>
+
+            <li class="sub-menu">
+                <a href="javascript:;" class="">
+                    <i class="icon_document_alt"></i>
+                    <span>Riwayat Hidup</span>
+                    <span class="menu-arrow arrow_carrot-right"></span>
+                </a>
+                <ul class="sub">
+                    <li><a class=""></a></li>
+                    <li></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+  </aside> --}}
