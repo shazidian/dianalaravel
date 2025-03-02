53cffa6d91f395966c217917476836f2760101cc
shazidian <158548646+shazidian@users.noreply.github.com>
2025-03-03 05:46:12 +0700

    acara19

---
+++ resources/views/upload.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload File Dengan Laravel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="row">
+        <div class="container">
+            <h2 class="text-center my-5">Upload File Dengan Laravel</h2>
+            <div class="col-lg-8 mx-auto my-5">
+                <!-- Peringatan Jika Error -->
+                @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+
+                <form action="{{ route('upload.proses') }}" method="POST" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <b>File Gambar</b><br/>
+                        <input type="file" name="file">
+                    </div>
+
+                    <div class="form-group">
+                        <b>Keterangan</b>
+                        <textarea class="form-control" name="keterangan"></textarea>
+                    </div>
+
+                    <input type="submit" value="Upload" class="btn btn-primary">
+                    <form action="{{ route('upload.proses') }}" method="POST"
+                     enctype="multipart/form-data">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
