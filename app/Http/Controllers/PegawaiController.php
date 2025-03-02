82ee68d57b23e02765ff40808a54721e4fb5bbc2
shazidian <158548646+shazidian@users.noreply.github.com>
2025-03-03 05:05:45 +0700

    acara18

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -10,6 +10,15 @@ class PegawaiController extends Controller
         return view('formulir');
     }
     public function proses(Request $request){
+        $messages = [
+            'required' => 'Input :attribute wajib diisi',
+            'min'=> 'Input :attribute harus diisi minimal :min karakter!',
+            'max'=>'Input  :attribute harus diisi maksimal :max karakter!',
+        ];
+        $this->validate($request, [
+            'nama'=> 'required|min:5|max:20',
+            'alamat'=> 'required|alpha',
+        ], $messages);
         $nama = $request->input('nama');
         $alamat = $request->input('alamat');
 
