8b66f1c394bdb6c5362a7a4bf343b9fb967c56d5
shazidian <158548646+shazidian@users.noreply.github.com>
2025-03-02 20:09:00 +0700

    acara14

--- resources/views/backend/pengalamankerja/create.blade.php
+++ resources/views/backend/pengalamankerja/create.blade.php
@@ -62,15 +62,19 @@
                     <div class="panel-body">
                         <div class="form">
                             <form class="form-validate form-horizontal" id="pengalaman_kerja_form" method="POST"
-                            action="{{ route('pengalaman_kerja.index') }}">
+                            action="{{ isset($pengalaman_kerja) ? route('pengalaman_kerja.update', $pengalaman_kerja->id) :
+                            route('pengalaman_kerja.store') }}">
                                 {!! csrf_field() !!}
+                                {!! isset($pengalaman_kerja) ? method_field('PUT') : '' !!}
+                                <input type="hidden" name="id" value="{{ $pengalaman_kerja->id }}"> <br/>
                                 {{-- nama perusahaan --}}
                                 <div class="form-group">
                                     <label for="cname" class="control-label col-lg-2">Nama Perusahaan <span
                                         class="required">*</span></label>
                                     <div class="col-lg-10">
                                         <input class="form-control" id="nama" name="nama" minlength="5" type="text"
-                                        required />
+                                        value="{{ isset($pengalaman_kerja) ? $pengalaman_kerja->nama : ''}}"
+                                            required />
                                     </div>
                                 </div>
                                {{-- nama jabatan --}}
@@ -79,7 +83,8 @@
                                         class="required">*</span></label>
                                     <div class="col-lg-10">
                                         <input class="form-control" id="jabatan" name="jabatan" minlength="2" type="text"
-                                        required />
+                                        value="{{ isset($pengalaman_kerja) ? $pengalaman_kerja->jabatan : ''}}"
+                                         required />
                                     </div>
                                 </div>
                                 {{-- tahun masuk --}}
@@ -88,6 +93,7 @@
                                         class="required">*</span></label>
                                     <div class="col-lg-10">
                                         <input id="tahun_masuk" type="text" name="tahun_masuk" class="form-control"
+                                        value="{{ isset($pengalaman_kerja) ? $pengalaman_kerja->tahun_masuk : ''}}"
                                         required>
                                     </div>
                                 </div>
@@ -97,6 +103,7 @@
                                         class="required">*</span></label>
                                     <div class="col-lg-10">
                                         <input id="tahun_keluar" type="text" name="tahun_keluar" class="form-control"
+                                        value="{{ isset($pengalaman_kerja) ? $pengalaman_kerja->tahun_keluar : ''}}"
                                         required>
                                     </div>
                                 </div>
