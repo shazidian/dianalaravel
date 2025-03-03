c9718f89ad8c9a47b840b0d0ef7d8eeef5a6bbb1
shazidian <158548646+shazidian@users.noreply.github.com>
2025-03-04 05:49:45 +0700

    acara 21-22

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiPendidikanController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('api_pendidikan', [ApiPendidikanController::class, 'getAll']);
+Route::get('api_pendidikan/{id}', [ApiPendidikanController::class, 'getPen']);
+Route::post('api_pendidikan', [ApiPendidikanController::class, 'createPen']);
+Route::put('api_pendidikan/{id}', [ApiPendidikanController::class, 'updatePen']);
+Route::delete('api_pendidikan/{id}', [ApiPendidikanController::class, 'deletePen']);
